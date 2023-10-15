101b514a1aa593c26ebc3df2d4c253e987eb8975
Daniel Castro <danicc097@gmail.com>
2023-10-15 12:41:12 +0200

    cleanup before synce

---
+++ beyond_typus/README.md
@@ -0,0 +1,7 @@
+# unit sizes
+
+0.35 seems to fit best since some provinces are minuscule.
+
+```bash
+find . -type f -name "*unit*.gfx" -exec sed -i -E 's/scale = 0\.[0-9]+/scale = 0.35/g' {} \;
+```
---
+++ scripts/get_dlc_units.ps1
@@ -0,0 +1,38 @@
+$sourceDirectory = "C:\Games\Europa Universalis IV\dlc"
+$tempPath = "D:\Downloads\temp_unit_pack_extraction"
+$destinationDirectory = "D:\Downloads\extracted_unit_packs"
+
+rm -rf $tempPath
+rm -rf $destinationDirectory
+mkdir $tempPath
+mkdir $destinationDirectory
+
+$folders = Get-ChildItem -Path $sourceDirectory -Directory | Where-Object { $_.Name -like "*unit_pack" }
+
+foreach ($folder in $folders) {
+  $zipFiles = Get-ChildItem -Path $folder.FullName -File -Filter *.zip
+
+  foreach ($zipFile in $zipFiles) {
+    $extractedFolder = Join-Path -Path $tempPath -ChildPath $folder.Name
+    if (-not (Test-Path -Path $extractedFolder -PathType Container)) {
+      New-Item -Path $extractedFolder -ItemType Directory
+    }
+
+    Add-Type -AssemblyName System.IO.Compression.FileSystem
+    [System.IO.Compression.ZipFile]::OpenRead($zipFile.FullName).Entries | ForEach-Object {
+      if ($_.FullName -like "interface/assets/*") {
+        $destinationPath = Join-Path -Path $extractedFolder -ChildPath $_.Name
+        $_.Open().CopyTo([System.IO.File]::Create($destinationPath))
+      }
+    }
+  }
+}
+
+$gfxFiles = Get-ChildItem -Path $tempPath -File -Recurse -Filter *.gfx
+rm -r $tempPath
+foreach ($gfxFile in $gfxFiles) {
+  $destinationPath = Join-Path -Path $destinationDirectory -ChildPath $gfxFile.Name
+  Write-Host "gfxFile.FullName" $gfxFile.FullName
+  Write-Host $destinationPath
+  Move-Item -Path $gfxFile.FullName -Destination $destinationPath -Force
+}
